6c71de84d0fc3eb8f9150a5cbb2a4296f60d80db
conrad96 <conrad.mugisha@gmail.com>
2019-09-27 19:51:15 +0300

    added some logic to display correctly selected button

--- app/src/main/java/com/example/swoosh/controller/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/controller/SkillActivity.kt
@@ -2,16 +2,36 @@ package com.example.swoosh.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.swoosh.utilities.EXTRA_LEAGUE
 import com.example.swoosh.R
+import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : AppCompatActivity() {
-    var league = ""
+    var league: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
         league = intent.getStringExtra(EXTRA_LEAGUE)
-        println(league)
+
+        //display selected value from previous screen
+        var selected = findViewById(R.id.selected_button) as TextView
+
+        if(league.contentEquals("mens")){
+            league = "Man"
+        }else if(league.contentEquals("womens")){
+            league = "Woman"
+        }
+
+        beginnerBtn.setOnClickListener{
+            ballerBtn.isChecked = false
+        }
+
+        ballerBtn.setOnClickListener {
+         beginnerBtn.isChecked = false
+        }
+
+        selected.text = "Iam a: "+league
     }
 }
--- app/src/main/java/com/example/swoosh/utilities/constants.kt
+++ app/src/main/java/com/example/swoosh/utilities/constants.kt
@@ -1,3 +1,4 @@
 package com.example.swoosh.utilities
 
 const val EXTRA_LEAGUE = "league"
+var PASSED_VALUE = ""

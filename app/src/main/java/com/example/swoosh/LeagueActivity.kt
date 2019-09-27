095abf2db676bb3f0c1445dbc4aadedacf2ab8d1
conrad96 <conrad.mugisha@gmail.com>
2019-09-27 18:15:13 +0300

    added click actionlisteners on league buttons and also included a toast incase no league is selected

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -3,17 +3,42 @@ package com.example.swoosh
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : AppCompatActivity() {
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
 
+        mensLeagueBtn.setOnClickListener {
+            womensLeagueBtn.isChecked = false
+            coedLeagueBtn.isChecked = false
+            selectedLeague = "mens"
+        }
+
+        womensLeagueBtn.setOnClickListener {
+            mensLeagueBtn.isChecked = false
+            coedLeagueBtn.isChecked = false
+            selectedLeague = "womens"
+        }
+
+        coedLeagueBtn.setOnClickListener {
+            womensLeagueBtn.isChecked = false
+            mensLeagueBtn.isChecked = false
+            selectedLeague = "co-ed"
+        }
+
+
         next_btn.setOnClickListener {
-            val skillIntent = Intent(this, SkillActivity::class.java)
-            startActivity(skillIntent)
+            if(selectedLeague != ""){
+                val skillIntent = Intent(this, SkillActivity::class.java)
+                startActivity(skillIntent)
+            }else{
+                Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
